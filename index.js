15bdbecab7a3d8aaf51741ee3539eb346a59966d
Andrew <andrew@pureaquasprings.com>
2022-04-04 09:59:04 +1000

    commit tree 7e123716d81ab86105adb626f50de9c1fa3a3141

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+//const { doc } = require("mocha/lib/reporters");
+
 // Your code goes here
+/*document.addEventListener("DOMContentLoaded", function() {
+    updateDOM()
+});
+
+function updateDOM() {
+    document.getElementById("text")
+    .innerHTML = "This is pretty cool!"
+};*/
+/*document.addEventListener("DOMContentLoaded", function() {
+    updateDOM()
+});
+
+function updateDOM() {
+    document.getElementById("text")
+    .innerHTML = "Andrew is a rockstar"
+
+};*/
+
+document.addEventListener("DOMContentLoaded", function() {
+    updateDOM()
+});
+
+function updateDOM() {
+    document.getElementById("text")
+    .innerHTML = "This is really cool!"
+};
