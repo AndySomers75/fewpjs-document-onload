fb598239b03192e2b133c1550e77a5323112ca09
Andrew <andrew@pureaquasprings.com>
2022-04-04 10:05:25 +1000

    completed

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
+//const { doc } = require("mocha/lib/reporters");
+
 // Your code goes here
+/*document.addEventListener("DOMContentLoaded", function() {
+    updateDOM()
+});
+
+function updateDOM() {
+    document.getElementById("text")
+    .innerHTML = "This is pretty cool!"
+};*/
+/*document.addEventListener("DOMContentLoaded", function() {
+    updateDOM()
+});
+
+function updateDOM() {
+    document.getElementById("text")
+    .innerHTML = "Andrew is a rockstar"
+
+};*/
+
+/*document.addEventListener("DOMContentLoaded", function() {
+    updateDOM()
+});
+
+function updateDOM() {
+    document.getElementById("text")
+    .innerHTML = "This is really cool!"
+};*/
+document.addEventListener("DOMContentLoaded", function() {
+    updateMe()
+});
+
+function updateMe() {
+    document.getElementById("text")
+    .innerHTML = "Andy bear with a sore head"
+
+};
